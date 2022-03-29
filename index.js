8f86ac361a3306d9152581739223ea401d667bc2
Alena Nemtsova <jelenatbs@gmail.com>
2022-03-29 17:27:17 +0300

    small changes

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 let audiModels = ["Q2", "Q3", "Q5", "Q7", "Q8"];
 let bmwModels = ["1 Series", "2 Series", "3 Series", "4 Series", "5 Series"];
 
-function selectMake(sender) { //sender - тот, кто отправляет действие, отправитель функции, имеет свои свойства, к которым мы можем обращаться 
+function selectMake(sender) {
     let optionsString = "";
 
     if(sender.options[sender.selectedIndex].value == "none") {
         optionsString += `<option>${"Модель"}</option>`;
     }
-
     if (sender.options[sender.selectedIndex].value == "audi") {
         for(let model of audiModels)
         optionsString += `<option>${model}</option>`;
     }
-
     if (sender.options[sender.selectedIndex].value == "bmw") {
         for(let model of bmwModels)
         optionsString += `<option>${model}</option>`;
     }
     document.getElementById("models").innerHTML = optionsString;
 }
-
 
 
 function getSelectedItems() {
